53bd60ad1f0d2cba82ffa05b0bf5b9b4d2339a9e
edgarmuyomba <edgarmatthew247@gmail.com>
2023-11-06 07:44:24 +0300

    feature: gameOver controls

--- src/js/views/attackEvent.js
+++ src/js/views/attackEvent.js
@@ -1,7 +1,11 @@
 function clickFeedback(data) { // data: { hit: true, cell: cell, gameOver: true }
     if (data["gameOver"]) {
         // end the game, last target is the loser!
-        console.log("Game Over!!!");
+        const boards = document.querySelector("div.boards");
+        boards.style = "display: none;";
+
+        const gameOver = document.querySelector("div.gameOver");
+        gameOver.style = "";
     } else {
         var cell = data["cell"];
         if (data["hit"]) {
